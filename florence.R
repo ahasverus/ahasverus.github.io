1c8852520fb19a71cd5d54a02552d403d985f7a4
ahasverus <nicolas.casajus@gmail.com>
2016-01-31 11:40:12 -0500

    Anniversaire Florence

---
+++ florence.R
@@ -0,0 +1,10 @@
+
+### Variable Florence
+age2flo <- 28
+
+### Boucle infinie d'anniversaire
+while (Sys.Date() <= as.Date('2017-02-02')){
+    cat(':) :) :) :) :) :) :) :)\n')
+    cat('Bonne fête, Florence !!!\n')
+    cat(paste('Joyeux', age2flo, 'ans !!!\n\n'))
+}
