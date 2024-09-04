62d385684f1126a97cbd59bb785c175dd4f5c0a6
Nicolas Casajus <rdev.nc@gmail.com>
2024-09-04 10:51:29 +0200

    fix css

--- R/make_experience.R
+++ R/make_experience.R
@@ -34,11 +34,11 @@ make_experience <- function(html){
       langs <- langs[which(langs %in% lang)]
       for (k in langs){
         asset <- paste0(asset, '    <h1 lang="', substr(k, 1, 2), '">')
-        asset <- paste0(asset, "&diams;&nbsp;", yaml$content[[i]]$job[["title"]][[k]])
+        asset <- paste0(asset, "&diams;&nbsp;&nbsp;", yaml$content[[i]]$job[["title"]][[k]])
         asset <- paste0(asset, '</h1>\n')
       }
     } else {
-      asset <- paste0(asset, '    <h1>', "&diams;&nbsp;", yaml$content[[i]]$job[["title"]], '</h1>\n')
+      asset <- paste0(asset, '    <h1>', "&diams;&nbsp;&nbsp;", yaml$content[[i]]$job[["title"]], '</h1>\n')
     }
 
 
